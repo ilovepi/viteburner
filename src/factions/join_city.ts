4cf4a39a61e7e571304cc8b5f572ab323e74e0a4
Paul Kirth <pk1574@gmail.com>
2023-04-23 16:02:38 -0700

    Use an external script to join cities

---
+++ src/factions/join_city.ts
@@ -0,0 +1,37 @@
+import { CityName, NS } from '@ns';
+import { waitForMoney, waitForInviteAndJoin } from '@/utils/utils';
+
+async function joinCity(ns: NS, city: CityName, amount: number) {
+  while (!ns.singularity.travelToCity(city)) {
+    await ns.sleep(100);
+  }
+  await waitForMoney(ns, amount);
+  await waitForInviteAndJoin(ns, city);
+}
+
+export async function main(ns: NS) {
+  const args = ns.flags([
+    ['help', false],
+    ['city', ns.enums.CityName.Sector12],
+    ['money', 15e6],
+  ]);
+  if (args.help) {
+    ns.tprint('Travel to a city and try to join their faction');
+    ns.tprint(`Usage: run ${ns.getScriptName()} SERVER`);
+    ns.tprint(`Example:`);
+    ns.tprint(`> run ${ns.getScriptName()} --target n00dles`);
+    return;
+  }
+
+  const CityMap = new Map([
+    ['Sector-12', ns.enums.CityName.Sector12],
+    ['Chongqing', ns.enums.CityName.Chongqing],
+    ['New Tokyo', ns.enums.CityName.NewTokyo],
+    ['Ishima', ns.enums.CityName.Ishima],
+    ['Aevum', ns.enums.CityName.Aevum],
+    ['Volhaven', ns.enums.CityName.Volhaven],
+  ]);
+  const city = CityMap.get(args.city.toString());
+  if (city === undefined) return;
+  await joinCity(ns, city, args.money);
+}
