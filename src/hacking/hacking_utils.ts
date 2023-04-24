7f8bdfa917c7a8e63381e2d9f4b3161198019b09
Paul Kirth <pk1574@gmail.com>
2023-04-24 09:44:00 -0700

    Describe how work threads are set in runScript
    I'll forget why in the next 10 minutes ...

--- src/hacking/hacking_utils.ts
+++ src/hacking/hacking_utils.ts
@@ -3,14 +3,16 @@ import { ServerHackAnalysis } from '@/servers/ServerHackAnalysis';
 import { NS } from '@ns';
 import { Scripts } from './hacking_constants';
 
+const hack_ratio = 0.1;
+
 export function attackServer(ns: NS, target: ServerData, workers: ServerData[]) {
-  const attack_data = new ServerHackAnalysis(ns, target, 0.1);
   // handle weaken first
   const script_ram = 1.75;
   const script = Scripts.weaken;
   const delta = 50;
   const grow_delay = target.weaken_time - target.grow_time;
   const hack_delay = target.weaken_time - target.hack_time;
+  const attack_data = new ServerHackAnalysis(ns, target, hack_ratio);
 
   const pids: number[] = [];
   pids.push(...runScript(target.name, workers, ns, 1.7, attack_data.threads.hack, Scripts.hack, hack_delay + delta));
@@ -23,7 +25,7 @@ export function attackServer(ns: NS, target: ServerData, workers: ServerData[]) 
 }
 
 export function prepareServer(ns: NS, target: ServerData, workers: ServerData[]) {
-  const attack_data = new ServerHackAnalysis(ns, target, 0.1);
+  const attack_data = new ServerHackAnalysis(ns, target, hack_ratio);
   // handle weaken first
   const weaken_threds = attack_data.threads.weaken_initial;
   const script_ram = 1.75;
@@ -54,8 +56,10 @@ export function runScript(
     const avail_threads = getAvailibleThreads(ns, server, script_ram);
     let threads_used;
     if (avail_threads > weaken_threads) {
+      // we have enough memory to run everything else, so just run them all
       threads_used = weaken_threads;
     } else {
+      // run as much as we can and let the other workers handle the rest...
       weaken_threads -= avail_threads;
       threads_used = avail_threads;
     }
@@ -64,15 +68,15 @@ export function runScript(
   return pids;
 }
 
-function getExploitArg(target: string, delay: number, sleep_time: number) {
+export function getExploitArg(target: string, delay: number, sleep_time: number) {
   return ['--target', target, '--delay', delay, '--sleep', sleep_time, '--id', id++];
 }
 
-function getAvailibleRam(ns: NS, server: ServerData) {
+export function getAvailibleRam(ns: NS, server: ServerData) {
   return server.ram - ns.getServerUsedRam(server.name);
 }
 
-function getAvailibleThreads(ns: NS, server: ServerData, script_ram: number) {
+export function getAvailibleThreads(ns: NS, server: ServerData, script_ram: number) {
   const availible_ram = getAvailibleRam(ns, server);
   return Math.floor(availible_ram / script_ram);
 }
